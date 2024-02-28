4f41473c8ee2d40d8ac3c02747f0704031170f90
Karan Kumar  Sahu <102219640+Karan09823@users.noreply.github.com>
2024-02-29 00:06:06 +0530

    Update UserDAO.java

--- Enotes_servlet_jsp_jdbc/src/main/java/com/DAO/UserDAO.java
+++ Enotes_servlet_jsp_jdbc/src/main/java/com/DAO/UserDAO.java
@@ -16,57 +16,56 @@ public class UserDAO {
 	}
 
 	public boolean addUser(UserData user) {
-		
-		boolean f=false;
-		
+
+		boolean f = false;
+
 		try {
-			
-			String query="insert into user(Name,Email,Password) values(?,?,?)";
-			PreparedStatement ps=con.prepareStatement(query);
-			
+
+			String query = "insert into user(Name,Email,Password) values(?,?,?)";
+			PreparedStatement ps = con.prepareStatement(query);
+
 			ps.setString(1, user.getName());
 			ps.setString(2, user.getEmail());
 			ps.setString(3, user.getPassword());
-			
-			int i=ps.executeUpdate();
-			
-			if(i==1) {
-				f=true;
+
+			int i = ps.executeUpdate();
+
+			if (i == 1) {
+				f = true;
 				/* System.out.printf("Number of rows affected:",i); */
 			}
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return f;
-		
 
 	}
-	
+
 	public UserData loginUser(UserData user) {
-		UserData usd=new UserData();
-		
+		UserData usd = new UserData();
+
 		try {
-			String query="select * from user where Email=? and Password=?";
-			PreparedStatement ps=con.prepareStatement(query);
-			
+			String query = "select * from user where Email=? and Password=?";
+			PreparedStatement ps = con.prepareStatement(query);
+
 			ps.setString(1, user.getEmail());
 			ps.setString(2, user.getPassword());
-			
-			ResultSet rs=ps.executeQuery();
-			
-			if(rs.next()) {
-				
-				usd.setName(rs.getString("Id"));
-				usd.setEmail(rs.getString("Email"));
-				usd.setPassword(rs.getString("Password"));
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				usd.setId(rs.getInt(1));
+				usd.setName(rs.getString(2));
+				usd.setEmail(rs.getString(3));
+				usd.setPassword(rs.getString(4));
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return usd;
 	}
 
